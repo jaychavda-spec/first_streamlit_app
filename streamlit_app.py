293c1be80c12aa5a90e3faaae8ac69b22e522070
jaychavda-spec <96901789+jaychavda-spec@users.noreply.github.com>
2023-05-05 16:21:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,6 @@ streamlit.header("jay chavda header")
 streamlit.text("jay chavda")
 streamlit.text("jay chavda")
 streamlit.text("jay chavda")
-streamlit.title("jay chavda header")
 streamlit.header("jay chavda title")
+streamlit.title("jay chavda header")
+
