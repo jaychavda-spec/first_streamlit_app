437abd7fd4000e4835ac1272b6ec58d3af2aa55b
jaychavda-spec <96901789+jaychavda-spec@users.noreply.github.com>
2023-05-05 16:32:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text("🍞 Dinner at 07:00 P.M.")
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.multiselect(list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 
-streamlist.multiselect(list(my_fruit_list.index))
+
