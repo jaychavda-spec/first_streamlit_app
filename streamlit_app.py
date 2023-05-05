22760bdb105de9273bae5ffbf0ae614e27e97573
jaychavda-spec <96901789+jaychavda-spec@users.noreply.github.com>
2023-05-05 16:21:25 +0530

    Update streamlit_app.py
    add title,header and text all of it

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
-streamlit.title("jay chavda")
+streamlit.title("jay chavda title")
+streamlit.header("jay chavda header")
+streamlit.text("jay chavda")
+streamlit.text("jay chavda")
+streamlit.text("jay chavda")
+streamlit.title("jay chavda header")
+streamlit.header("jay chavda title")
